4485682a0c38197cba79466ad6820a283b073d3c
Tom Kooij <tomkooij@tomkooij.nl>
2016-04-06 12:51:50 +0200

    setup vim for %edit

--- .ipython/ipython_config.py
+++ .ipython/ipython_config.py
@@ -8,3 +8,6 @@ c.InteractiveShellApp.exec_lines.append('from __future__ import division')
 #c.InteractiveShellApp.exec_lines.append('from __future__ import with_statement')
 c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
 c.InteractiveShellApp.exec_lines.append('%autoreload 2')
+# editor
+
+c.TerminalInteractiveShell.editor = '/usr/bin/env vim'
